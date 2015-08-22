1affc038762a134b9d147849bf85345e4e8758ba
hanxi <hanxi.info@gmail.com>
2015-08-22 11:55:53 +0800

    support android studio

--- build_native.sh
+++
@@ -1,9 +0,0 @@
-ndk-build NDK_MODULE_PATH=jni
-#ant release
-#adb uninstall com.hanxi.luarun
-#adb install bin/luaIDE-release.apk
-#adb logcat
-ant debug
-adb uninstall com.hanxi.luarun
-adb install bin/luaIDE-debug-unaligned.apk
-#adb logcat
